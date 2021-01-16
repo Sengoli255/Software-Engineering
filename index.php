5b53caa090328685fabc06f88b6fd42d747b463d
mamu12-creator <72909327+mamu12-creator@users.noreply.github.com>
2021-01-16 13:40:38 +0300

    interface that has option for staff/admin to login

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food Ordering System</title>
+	<style>
+		.head2{
+		
+		  width: 15%;
+		  height: 5%;
+		  padding: 1%;
+		  margin-top: 5%;
+		  margin-left: 70%;
+		  margin-right: 0.1%;
+		  float: left;
+		  
+		  opacity:0.9;
+		}
+		.head3{
+			width: 15%;
+		  height: 10%;
+		  padding: 1%;
+		  margin-top: 5%;
+		  margin-left: 70%;
+		  margin-right: 0.1%;
+		  float: left;
+		  
+		  opacity:0.9;
+		}
+		a{
+			color:white;
+			font-size:25px;
+		}
+		a:link {
+		  text-decoration: none;
+		 
+		}
+		.btn{
+			width: 100%;
+		  height: 100%;
+		  font-size:25px;
+		   border-radius: 25px;
+		   background:#eb346b;
+		   color:white;
+			
+		}
+		button:hover {
+		  background-color:gray ;
+		 
+		}
+		body{
+			width:100%;
+			height:100%;
+			background-image: url(image/p8.jpg);background-repeat:no-repeat;width:100%;height:100%;
+		}
+		
+		.head{
+			margin-top: -1%;
+			color:white;
+			background-color:#eb346b;
+		}
+		
+	</style>				
+</head>
+<body >
+
+<div class="head">
+<hr>
+<h1 style="text-align:center;">DeliciousFood Restaurant</h1>
+<hr>
+</div>
+
+<div class="head2">
+	 <button class="btn" onclick="window.location.href='staff'">Staff Login</button></div>
+	<div class="head3">
+	 <button class="btn" onclick="window.location.href='admin'">Admin Login</button>
+	</div>
+	
+</body>
+</html>
